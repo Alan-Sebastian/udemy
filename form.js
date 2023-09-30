41a1d28808bc3e3d2c4f308d8127cf6fa7a4db1a
Alan Sebastian <95478024+Alan-Sebastian@users.noreply.github.com>
2023-09-30 08:05:52 -0700

    Add files via upload

--- form.js
+++ form.js
@@ -1,77 +1,15 @@
-document.body.addEventListener('keydown',function(e){
-    var keycode=e.keyCode;
-    if(keycode ==13){
-        console.log(keycode +"key down");
-    }
-})
+var user=document.getElementById("username")
+var error=document.getElementById('err')
+user.addEventListener('input',function(e){
+    // console.log(e.target.value)
+    var regex=/^[\w]{6,8}$/;
+    var currentvalue=e.target.value
+   var valid=regex.test(currentvalue)
+   if(valid){
+    error.style.display='none'
 
-document.body.addEventListener('keyup',function(e){
-    var keycode=e.keyCode;
-    if(keycode ==13){
-        console.log(keycode +"key up");
-    }
+   }
+   else{
+    error.style.display='block'
+   }
 })
-
-document.body.addEventListener('keypress',function(e){
-    var keycode=e.keyCode;
-    if(keycode ==13){
-        console.log(keycode +"key press");
-    }
-})
-//mouse events
-var mouse=document.getElementById("btn");
-mouse.addEventListener('mousedown',function(){
-    console.log("mouse down");
-})
-mouse.addEventListener('mouseup',function(){
-    console.log("mouse up");
-})
-mouse.addEventListener('click',function(){
-    console.log("click");
-})
-mouse.addEventListener('dblclick',function(){
-    console.log("double click");
-})
-//mouse over
-var over=document.getElementById("username");
-var overcount=0;
-over.addEventListener('mouseover',function(){
-    overcount+=1;
-    console.log(overcount)
-})
-//mouse enter
-var enter=document.getElementById("username");
-var entercount=0;
-enter.addEventListener('mouseenter',function(){
-    entercount+=1;
-    console.log(entercount+"mouse enter");
-})
-//mouse move
-var enter=document.getElementById("username");
-var entercount=0;
-enter.addEventListener('mousemove',function(){
-    entercount+=1;
-    console.log(entercount+"mouse move");
-})
-var user=document.getElementById("username");
-var submited=document.getElementById("btn-submit");
-username.addEventListener("change",function(){
-console.log("changed");
-});
-username.addEventListener('input',function(evn){
-    var current=evn.target.value;
-       current=current.toUpperCase();
-    //    username.value=current;
-       console.log(current);
-
-})
-password.addEventListener("focus",function(){
-    console.log("atleast 8 characters")
-})
-username.addEventListener("blur",function(){
-    console.log("focus lost")
-})
-submited.addEventListener("submit",function(e){
-    alert('submitted successfully');
- e.PreventDefault();
-});
